85eb4e837e9b51cedd8b4388eb64c4e90f4dede4
Dima Snisarenko <snisarenkodima@gmail.com>
2021-05-08 19:18:30 +0200

    feat: render img-comparison-slider

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ImgComparisonSlider>
+    <img
+        slot="before"
+        style="width: 100%"
+        src="https://sneas.github.io/img-comparison-slider/demo/images/cat.jpg"
+    />
+    <img
+        slot="after"
+        style="width: 100%"
+        src="https://sneas.github.io/img-comparison-slider/demo/images/dog.jpg"
+    />
+  </ImgComparisonSlider>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ImgComparisonSlider } from '@img-comparison-slider/vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ImgComparisonSlider
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
